028042e1e70b392f259d45c474062063ce1d2894
poignant-minifier-0983 <davidfano@gmail.com>
2019-06-30 23:51:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -88,4 +88,14 @@ class Student
     end
   end
 
+  def self.first_X_students_in_grade_10(number)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      -- ORDER BY student.id
+      LIMIT ?
+    SQL
+  end
+
 end
